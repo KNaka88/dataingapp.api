bf755caab7db2bf6088b2b90c9584af3a23c053e
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-01-03 11:31:29 -0800

    add introduction field

--- Dtos/UserForListDto.cs
+++ Dtos/UserForListDto.cs
@@ -9,6 +9,7 @@ namespace DatingApp.Api.Dtos
         public string Gender { get; set; }
         public int Age { get; set; }
         public string KnownAs { get; set; }
+        public string Introduction { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string City { get; set; }
