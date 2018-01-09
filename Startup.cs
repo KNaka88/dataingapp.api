96fcbc3efa1645a35c7766b7af2abb922e25c10d
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-01-08 23:58:38 -0800

    add controller for angular files

--- Startup.cs
+++ Startup.cs
@@ -100,7 +100,14 @@ namespace DatingApi.API
                     .AllowCredentials();
             });
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
