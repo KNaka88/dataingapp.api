03bb7b8ab3459422d2c3d3603169969e3bfe51bb
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-01-09 12:16:51 -0800

    add mysql as production environment

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,38 @@ namespace DatingApi.API
         {
             // Configuring a Db Context, adding the Dbcontext to dependency injection
             services.AddDbContext<DataContext>(options => options
+                .UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+                .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning)));
+            //https://docs.microsoft.com/en-us/ef/core/miscellaneous/configuring-dbcontext
+
+            services.AddTransient<Seed>();
+            services.AddCors();
+            services.AddMvc().AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
+            services.AddAutoMapper();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddScoped<LogUserActivity>();
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
+            // for JWT token authentication
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            // Configuring a Db Context, adding the Dbcontext to dependency injection
+            services.AddDbContext<DataContext>(options => options
                 .UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
                 .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning)));
             //https://docs.microsoft.com/en-us/ef/core/miscellaneous/configuring-dbcontext
